7023cab71dae9af5e5319a7dcf5180ab6ca9753e
edwinghu <123456>
2021-01-11 20:13:35 +0800

    提交代码

--- src/main/java/com/example/liquibase/LiquibaseApplication.java
+++ src/main/java/com/example/liquibase/LiquibaseApplication.java
@@ -3,11 +3,14 @@ package com.example.liquibase;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collections;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 @SpringBootApplication
 public class LiquibaseApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LiquibaseApplication.class, args);
 	}
-
 }
---
+++ src/main/java/com/example/liquibase/Test.java
@@ -0,0 +1,10 @@
+package com.example.liquibase;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("1111");
+    }
+}
